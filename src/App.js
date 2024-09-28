c0a34c2f2aa5f0e77859a2ccbcd41338145f4ece
Denys Severin <severindenys367@gmail.com>
2024-09-28 12:17:44 +0200

    adding extra path firebase + encounter scss problem unfixed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import { useEffect, useState } from "react";
 import { db } from "./db";
 import { getDoc, updateDoc, doc } from "firebase/firestore";
-
-import './App.css';
-
-const getUserId = () => window.location.host.split('.')[0];
-
+import "./styles/main.scss";
+const getUserId = () => window.location.host.split(".")[0];
 function App() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const docRef = doc(db, "test", getUserId());
+        const docRef = doc(db, "general_info", getUserId());
         const docSnap = await getDoc(docRef);
         if (docSnap.exists()) {
           const data = docSnap.data();
@@ -25,32 +22,21 @@ function App() {
     fetchData();
   }, []);
 
-  // const changeData = async () => {
-  //   const docRef = doc(db, "test", data.id);
-  //
-  //   try {
-  //     const newDocument = { title: "Updated title" , description: "HOw are u?"};
-  //     await updateDoc(docRef, newDocument);
-  //
-  //     setData({ ...data, ...newDocument });
-  //
-  //   } catch (error) {
-  //     console.error("Error updating document: ", error);
-  //   }
-  // }
+  console.log("data", data);
 
   return (
     <div className="App">
-      <h1>PUBLIC</h1>
-      {
-        data?.title && <h1> {data.title}</h1>
-      }
-      {
-          data?.description && <h2> {data.description}</h2>
-      }
-      {
-          data?.img_url && <img src={data.img_url}/>
-      }
+      {/* <h1>PUBLIC</h1> */}
+      {data?.about_me_title && <h1> {data.about_me_title}</h1>}
+      {data?.main_picture1 && <img src={data.main_picture1} />}
+      {data?.main_title && <h2> {data.main_title}</h2>}
+
+      {data?.about_me_sub_title && (
+        <h2> {data.about_me_sub_title}</h2>
+      )}
+      {data?.about_me_description && (
+        <h2> {data.about_me_description}</h2>
+      )}
     </div>
   );
 }
