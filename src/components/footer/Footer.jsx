ca74107e47ec9f92fd9568205c12815d1ebc29b2
Denys Severin <severindenys367@gmail.com>
2024-10-30 12:26:10 +0100

    adding check condition for all buttons and info in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,37 +4,55 @@ import linkedIn from "../../images/icons8-linkedin.svg";
 import whatsUp from "../../images/icons8-whatsapp.svg";
 
 export default function Footer({ footerData }) {
-  console.log(footerData);
+  console.log("footerData", footerData);
   return (
     <>
       <div className="footer">
         <div className="footer__container-follow">
-          <h3>write Me</h3>
+          <h3>Write Me</h3>
           <div className="follow__icons">
-            <a href="mailto:severindenys367@gmail.com">
-              <img src={gmailIcon} alt="gmail" />
-            </a>
-            <img src={linkedIn} alt="linkedin" />
-            <img src={whatsUp} alt="whatsUp" />
+            {footerData?.footer_contact_me?.gmail.trim() && (
+              <a
+                href={`mailto:${footerData?.footer_contact_me?.footer_email}`}
+              >
+                <img src={gmailIcon} alt="gmail" />
+              </a>
+            )}
+
+            {footerData?.footer_contact_me?.linkedin.trim() && (
+              <a
+                href={`https://www.linkedin.com/in/${footerData?.footer_contact_me?.linkedin}/`}
+              >
+                <img src={linkedIn} alt="linkedin" />
+              </a>
+            )}
+
+            {footerData?.footer_contact_me?.whatsapp.trim() && (
+              <a
+                href={`https://wa.me/${footerData?.footer_contact_me?.whatsapp.trim()}/?text=Welcome to my whatsapp `}
+              >
+                <img src={whatsUp} alt="whatsUp" />
+              </a>
+            )}
           </div>
         </div>
         <div className="footer__container-contact">
           <h3>Contact Me</h3>
           <div className="info">
             <div className="info__address">
-              {footerData && (
+              {footerData?.footer_contact_me?.footer_address?.trim() && (
                 <p>{footerData.footer_contact_me.footer_address}</p>
               )}
             </div>
 
             <div className="info__phone">
-              {footerData && (
+              {footerData?.footer_contact_me?.footer_phone?.trim() && (
                 <p>{footerData.footer_contact_me.footer_phone}</p>
               )}
             </div>
 
             <div className="info__email">
-              {footerData && (
+              {footerData?.footer_contact_me?.footer_email?.trim() && (
                 <p>{footerData.footer_contact_me.footer_email}</p>
               )}
             </div>
