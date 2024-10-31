44240ab7a3f7e006a0621c3ff4317c930276c930
Denys Severin <severindenys367@gmail.com>
2024-10-31 10:00:03 +0100

    display cross btn on hamburger menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,33 +13,49 @@ const Navbar = () => {
       style={{ height: isOpen ? "20rem" : "5rem" }}
     >
       <div className="hamburgerIcon" onClick={toggleHamburger}>
-        <svg
-          className="hamburgerIconBtn"
-          width="24px"
-          height="24px"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M4 18L20 18"
-            stroke="#000000"
-            strokeWidth="2"
-            strokeLinecap="round"
-          />
-          <path
-            d="M4 12L20 12"
-            stroke="#000000"
-            strokeWidth="2"
-            strokeLinecap="round"
-          />
-          <path
-            d="M4 6L20 6"
-            stroke="#000000"
-            strokeWidth="2"
-            strokeLinecap="round"
-          />
-        </svg>
+        {!isOpen ? (
+          <svg
+            className="hamburgerIconBtn"
+            width="24px"
+            height="24px"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M4 18L20 18"
+              stroke="#000000"
+              strokeWidth="2"
+              strokeLinecap="round"
+            />
+            <path
+              d="M4 12L20 12"
+              stroke="#000000"
+              strokeWidth="2"
+              strokeLinecap="round"
+            />
+            <path
+              d="M4 6L20 6"
+              stroke="#000000"
+              strokeWidth="2"
+              strokeLinecap="round"
+            />
+          </svg>
+        ) : (
+          <svg
+            className="hamburgerIconBtnCross"
+            fill="#000000"
+            height="10px" // Adjusted size
+            width="10px"
+            viewBox="0 0 490 490"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <polygon
+              points="456.851,0 245,212.564 33.149,0 0.708,32.337 212.669,245.004 0.708,457.678 33.149,490 245,277.443 456.851,490 
+	489.292,457.678 277.331,245.004 489.292,32.337"
+            />
+          </svg>
+        )}
       </div>
 
       <ul className={isOpen ? "navbar__open" : "navbar__close"}>
